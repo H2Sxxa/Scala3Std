2b101ea6cb976c6a727380c0d6ab7b4f77c862e9
H2Sxxa <h2sxxa0w0@gmail.com>
2024-09-30 22:12:36 +0800

    😭: Better Runtime Getter

--- src/main/scala/io/github/h2sxxa/scala3std/Scala3Std.scala
+++ src/main/scala/io/github/h2sxxa/scala3std/Scala3Std.scala
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.LogManager
 
 import scala.util.Properties
 
-
 @Mod(
   modid = "scala3std",
   name = "Scala3Std",
@@ -19,10 +18,16 @@ object Scala3Std {
 
   @Mod.EventHandler
   def init(event: FMLInitializationEvent): Unit = {
-    LOGGER.info("ScalaStd Loaded, Scala Runtime is")
-    LOGGER.info(Properties.versionString)
-    for msg <- List("This msg is printed by Scala3 Indent Grammar")
-      do
-        LOGGER.info(msg)
+    val resources = ClassLoader.getSystemClassLoader.getResources("library.properties")
+    LOGGER.info("Detecting Scala Libraries...")
+    while resources.hasMoreElements
+    do
+      val element = resources.nextElement()
+      try {
+        val props = java.util.Properties()
+        props.load(element.openStream())
+        LOGGER.info(element.getPath + " version:" + props.getProperty("version.number"))
+      } catch
+        case _ => ()
   }
 }
