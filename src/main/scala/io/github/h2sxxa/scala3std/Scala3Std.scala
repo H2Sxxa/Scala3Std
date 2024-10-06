6cd08ce1fdec190c889066c90470c8dc099fda20
H2Sxxa <h2sxxa0w0@gmail.com>
2024-10-06 19:25:13 +0800

    :sparkles: compat for higher version of forge

--- src/main/scala/io/github/h2sxxa/scala3std/Scala3Std.scala
+++ src/main/scala/io/github/h2sxxa/scala3std/Scala3Std.scala
@@ -1,8 +1,6 @@
 package io.github.h2sxxa.scala3std
 
-import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.Mod.EventHandler
-import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import cross.net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
 
 import scala.annotation.unused
@@ -13,25 +11,23 @@ import scala.util.Properties
   value = "scala3std",
   modid = "scala3std",
   name = "Scala3Std",
-  version = "3.5.1+A",
+  version = "3.5.1+B",
   modLanguage = "scala",
 )
 object Scala3Std {
   private val LOGGER = LogManager.getLogger("Scala3Std")
 
-  @unused
-  @Mod.EventHandler
-  def init(@unused event: FMLInitializationEvent): Unit = {
-    val resources = classOf[Option[?]].getClassLoader.getResources("library.properties")
-    LOGGER.info("Detecting Scala Libraries...")
-    while resources.hasMoreElements
-    do
-      val element = resources.nextElement()
-      try {
-        val props = java.util.Properties()
-        props.load(element.openStream())
-        LOGGER.info(element.getPath + " version:" + props.getProperty("version.number"))
-      } catch
-        case _ =>
-  }
+
+  private val resources = classOf[Option[?]].getClassLoader.getResources("library.properties")
+  LOGGER.info("Detecting Scala Libraries...")
+  while resources.hasMoreElements
+  do
+    val element = resources.nextElement()
+    try {
+      val props = java.util.Properties()
+      props.load(element.openStream())
+      LOGGER.info(element.getPath + " version:" + props.getProperty("version.number"))
+    } catch
+      case _ =>
+  
 }
